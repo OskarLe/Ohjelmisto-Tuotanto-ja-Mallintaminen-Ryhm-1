01d86476a30aaadff78ac0ac152f51cdd1712fa5
mmakiri <miska.makirinta@edu.turkuamk.fi>
2021-04-19 01:49:29 +0300

    Streamline

--- surveyapp/src/App.test.js
+++ surveyapp/src/App.test.js
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
 
-test("renders learn react link", () => {
+test("If it works", () => {
   render(
     <BrowserRouter>
       <App />
--- surveyapp/src/components/Survey.js
+++ surveyapp/src/components/Survey.js
@@ -1,19 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ReCAPTCHA from "react-google-recaptcha";
-
-function onChange(value) {
-  console.log("Captcha value:", value);
-}
 
 const Survey = () => (
   <div>
     SURVEY
-    <ReCAPTCHA 
-    sitekey="6LcLLK4aAAAAANw_py0M0qaU8oRyS4paL2MW7-Fl" 
-    onChange={onChange} 
-    theme="dark"
-    />
     <p>
       <Link to="/Home">
         <button className="Button" type="button">
