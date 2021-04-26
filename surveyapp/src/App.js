292a69afc7dba57b30ffc3fadc6de1e0dca9387c
mmakiri <miska.makirinta@edu.turkuamk.fi>
2021-04-26 23:27:01 +0300

    Streamline

--- surveyapp/src/App.js
+++ surveyapp/src/App.js
@@ -6,8 +6,6 @@ import { Route, Switch, Redirect,} from "react-router-dom";
 import Home from "./components/Home";
 import SurveyPage_fi from "./components/Survey_fi";
 import SurveyPage_en from "./components/Survey_en";
-import SurveyCompleted_fi from "./components/SurveyCompleted_fi";
-import SurveyCompleted_en from "./components/SurveyCompleted_en";
 
 class App extends Component {
   render() {
@@ -19,8 +17,6 @@ class App extends Component {
           <Route path="/Survey_fi" component={SurveyPage_fi} />
           <Route path="/Survey_en" component={SurveyPage_en} />
 
-          <Route path="/SurveyCompleted_fi" component={SurveyCompleted_fi} />
-          <Route path="/SurveyCompleted_en" component={SurveyCompleted_en} />
           <Redirect to="/" />
         </Switch>
       </div>
